bbbd6a77a3d8bdc7d57fc4a08f2ad156c4719727
Patrick Xiao <pdxiao2@illinois.edu>
2026-03-17 14:10:13 -0700

    chore: added Dockerfiles

--- scraper-service/kafka_client.py
+++ scraper-service/kafka_client.py
@@ -1,14 +1,20 @@
 import json
-from kafka import KafkaProducer
 import os
 from dotenv import load_dotenv
+from kafka import KafkaProducer
 
 load_dotenv()
 
-producer = KafkaProducer(
-    bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092"),
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
-)
+_producer = None
+
+def _get_producer():
+    global _producer
+    if _producer is None:
+        _producer = KafkaProducer(
+            bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092"),
+            value_serializer=lambda v: json.dumps(v).encode("utf-8")
+        )
+    return _producer
 
 def publish_job(job: dict):
-    producer.send("jobs.new", value=job)
+    _get_producer().send("jobs.new", value=job)
